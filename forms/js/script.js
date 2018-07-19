1eecaa22fb4e37a2f9b8ebc3f47b16c4d236c856
Oleksandr Demianov <odemiatc@training.local>
2018-07-19 21:16:42 +0300

    withJS2

--- forms/js/script.js
+++ forms/js/script.js
@@ -1,4 +1,7 @@
+localStorage["possitive"]=0;
+localStorage["negative"]=0
 submit.addEventListener('click', function(e) {
+    
     var firstNumber = Number(first.value);  // read value from input with id="str"
     var secondNumber = Number(second.value);  // read value from input with id="str"
     var checkingNumber= Number(checking.value);
@@ -8,21 +11,26 @@ submit.addEventListener('click', function(e) {
 
 
     
-var action = math.options[math.selectedIndex].text;
-var sign;
+var action = math.options[math.selectedIndex].value;
+var expected;
 switch(action){
-case 'plus'  : sign = firstNumber + secondNumber; break;
-case 'minus' : sign = firstNumber - secondNumber; break;
-case 'multi' : sign = firstNumber * secondNumber; break;
-case 'division':sign= firstNumber / secondNumber; break;
+case 'plus' : expected = firstNumber + secondNumber; break;
+case 'minus' : expected = firstNumber - secondNumber; break;
+case 'multi' : expected = firstNumber * secondNumber; break;
+case 'division' : expected = firstNumber / secondNumber; break;
 }
-alert(sign);
-  if (checkingNumber==sign) {
-      document.getElementById("result").innerHTML="CORRECT";
-      document.getElementById("grade").style.backgroundColor="green"}
-      else {
-    document.getElementById("result").innerHTML="WRONG";
-    document.getElementById("grade").style.backgroundColor="red";
-    document.getElementById("grade").style.borderWidth="2 px";
-};
+
+
+   if (checkingNumber==expected) {
+       result.innerHTML=localStorage["possitive"];//"CORRECT";
+       localStorage["possitive"] = Number(localStorage["possitive"])+1;
+       document.getElementById("grade").style.backgroundColor="green"  ;  
+       
+    }
+    else {
+     document.getElementById("result").innerHTML="WRONG";
+     localStorage["negative"] = Number(localStorage["negative"])+1;
+     document.getElementById("grade").style.backgroundColor="red" ; 
+ };
+ document.getElementById("storeCorrect").innerHTML=localstore["possitive"];
 });
